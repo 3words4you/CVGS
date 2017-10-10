31062a6bcbd336d1de4b9684bd3846a54a8152db
LAPTOP-I39LVNAT\moyi0 <1197967396@qq.com>
2017-10-09 22:44:42 -0400

    add game,event,user ctl, no edit

---
+++ CVGS/Controllers/GamesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CVGS.Models;
+
+namespace CVGS.Controllers
+{
+    public class GamesController : Controller
+    {
+        private CVGSEntities db = new CVGSEntities();
+
+        // GET: Games
+        public ActionResult Index()
+        {
+            var games = db.Games.Include(g => g.Category).Include(g => g.Category1);
+            return View(games.ToList());
+        }
+
+        // GET: Games/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // GET: Games/Create
+        public ActionResult Create()
+        {
+            ViewBag.categoryID = new SelectList(db.Categories, "categoryID", "categoryName");
+            ViewBag.subCategoryID = new SelectList(db.Categories, "categoryID", "categoryName");
+            return View();
+        }
+
+        // POST: Games/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "gameID,title,description,categoryID,subCategoryID,developer,publisher,releasedDate,createdDate,updatedDate,price")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Games.Add(game);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.categoryID = new SelectList(db.Categories, "categoryID", "categoryName", game.categoryID);
+            ViewBag.subCategoryID = new SelectList(db.Categories, "categoryID", "categoryName", game.subCategoryID);
+            return View(game);
+        }
+
+        // GET: Games/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.categoryID = new SelectList(db.Categories, "categoryID", "categoryName", game.categoryID);
+            ViewBag.subCategoryID = new SelectList(db.Categories, "categoryID", "categoryName", game.subCategoryID);
+            return View(game);
+        }
+
+        // POST: Games/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "gameID,title,description,categoryID,subCategoryID,developer,publisher,releasedDate,createdDate,updatedDate,price")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(game).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.categoryID = new SelectList(db.Categories, "categoryID", "categoryName", game.categoryID);
+            ViewBag.subCategoryID = new SelectList(db.Categories, "categoryID", "categoryName", game.subCategoryID);
+            return View(game);
+        }
+
+        // GET: Games/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // POST: Games/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Game game = db.Games.Find(id);
+            db.Games.Remove(game);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
